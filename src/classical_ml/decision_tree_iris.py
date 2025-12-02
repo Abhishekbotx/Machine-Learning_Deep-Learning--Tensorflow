5fde7c916e4b0b0ea95fa3ecd2c91920348d1376
Abhishekbotx <abhishekbotx@gmail.com>
2025-12-02 08:16:32 +0530

    setted up tf with dedicated gpu and minor changes

--- src/classical_ml/decision_tree_iris.py
+++ src/classical_ml/decision_tree_iris.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import json
 
-# 1️⃣ Load dataset
+# 1. Load dataset
 data = load_iris()
 # print("data::",data)
 X = data.data       # features (measurements)sepal length, sepal width, petal length, petal width
@@ -14,36 +14,36 @@ y = data.target     # labels (flower species) encoded in 0,1,2 indexing
 
 
 
-# 2️⃣ Split into train and test
+# 2️. Split into train and test
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=26 # 0.2 = 20% test data, 80% train data
 )                                   # 42 gives most of the time same prediction thats why we are using 42
 
 # print("x_train:",X_train,"X_test:",X_test,"y_train:",y_train,"y_test:",y_test)
 
-# 3️⃣ Train model
+# 3️. Train model
 model = DecisionTreeClassifier() #We create a Decision Tree classifier
 model.fit(X_train, y_train) #.fit() means learn patterns from training data
 
-# 4️⃣ Predict
+# 4️. Predict
 y_pred = model.predict(X_test)
 
 print("y_test:",y_test)
 print("y_pred:",y_pred)
 
-# 5️⃣ Evaluate
+#  Evaluate
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
 
-# ✅ Confusion Matrix
+#  Confusion Matrix
 print("Confusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 
-# ✅ Classification Report
+#  Classification Report
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
 
-# ✅ Visualize Tree
+#  Visualize Tree
 plt.figure(figsize=(10,8))
 plot_tree(model, filled=True, class_names=data.target_names, feature_names=data.feature_names)
 plt.show()
